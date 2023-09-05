40558267a8d6bbe50d4942cc823acc22a78131e0
markup54 <marzenakupka@tlen.pl>
2023-09-05 10:34:01 +0200

    wypisywanie informacji na ekranie

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    //nazwa pliku taka jak nazwa klasy
+    //zwyczajowo klasa z wielkiej litery
+    /*public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+     */
+    public static void main(String[] args) {
+        System.out.println("Pierwszy program w Javie");
+    }
+}
