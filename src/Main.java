38e42b4c7e1563864cad7d7167718866d6eea96f
markup54 <marzenakupka@tlen.pl>
2023-09-05 12:16:30 +0200

    losowanie 6 liczb do tablicy

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //losowanie sześciu liczb
 
+        //wylosowane liczby zapiszemy w tablicy
+        //tablica w Javie może zawierać tylko typy proste
+
+        System.out.println("Witamy, wylosujemy 6 liczb");
+        int wylosowane [] = new int[6];
+        for (int i =0; i <wylosowane.length;i++){
+            wylosowane[i] = (int)(Math.random()*100+1);
+            System.out.println(wylosowane[i]);
+        }
 
     }
 
