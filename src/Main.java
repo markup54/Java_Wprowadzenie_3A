7c5554a779bed70b580dbae65de5a800e6638de4
markup54 <marzenakupka@tlen.pl>
2023-09-05 13:07:09 +0200

    metoda wylosuj

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,19 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //losowanie sześciu liczb
-
-        //wylosowane liczby zapiszemy w tablicy
-        //tablica w Javie może zawierać typy proste i złożone
-        //tablica nie może zmieniać rozmiaru w trakcie działania programu
-
-        System.out.println("Witamy, wylosujemy 6 liczb");
-        int wylosowane [] = new int[6];
-        for (int i =0; i <wylosowane.length;i++){
-            wylosowane[i] = (int)(Math.random()*100+1);
-            System.out.println(wylosowane[i]);
-        }
-        //losowanie liczb do kolekcji
-        /*
-        kolekcje:
-        listy - ArrayList, LinkedList elementy uporządkowane, indeksowane, mogą się powtarzać
-        set - HashSet, elementy najczęściej nieindeksowane, nieuporządkowany, nie mogą się powtarzać
-
-         */
-        System.out.println("Losowanie 5 liczb do listy");
+    private static Set<Integer> wylosuj(){
+        System.out.println("Losowanie 6 liczb do listy");
         HashSet<Integer> wylosowaneLiczby = new HashSet<>();
-        Integer liczba; // typ złożony liczba to obiekt ma metody
-        int liczba2;//typ prosty liczba2 nie ma metod to tylko zmienna
-
         while(wylosowaneLiczby.size()<6){
             int liczbaLosowana = (int)(Math.random()*10+1);
             wylosowaneLiczby.add(liczbaLosowana);
         }
+        return wylosowaneLiczby;
+    }
+    public static void main(String[] args) {
+
+        Set<Integer> wylosowaneLiczby;
+        wylosowaneLiczby = wylosuj();
         System.out.println("Wylosowane Liczby"+ wylosowaneLiczby);
         //wczytywanie 6 liczb z klawiatury bez powtórzeń
         ArrayList<Integer> wczytaneLiczby = new ArrayList<>();
